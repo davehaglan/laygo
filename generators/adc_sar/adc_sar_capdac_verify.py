6cfcf9a0a248de2916bb2a76ec2b31b47b5ca14a
jdhan <jdhan@eecs.berkeley.edu>
2017-07-21 08:45:12 -0700

    capdac

--- generators/adc_sar/adc_sar_capdac_verify.py
+++ generators/adc_sar/adc_sar_capdac_verify.py
@@ -3,7 +3,6 @@
 import pprint
 
 import bag
-#from laygo import *
 import laygo
 import numpy as np
 import yaml
@@ -11,20 +10,20 @@ import matplotlib.pyplot as plt
 
 lib_name = 'adc_sar_templates'
 cell_name = 'capdac'
+tb_cell_name = 'capdac_tb_tran'
 impl_lib = 'adc_sar_generated'
-tester_cell_name = 'capdac_tester' #intermediate cell for programmable testbench generation
-tb_lib = 'adc_sar_testbenches'
-#tb_cell = 'capdac_8b_tb_tran'
-tb_cell = 'capdac_tb_tran'
 
-#spec
+#spec and parameters
 vh=0.3
 vl=0.0
 vcm=0.15
 per=1e-9
-num_bits=8
+params = dict(
+    num_bits = 8,
+    c_m = 1,
+    rdx_array = [1, 2, 4, 8, 16, 32, 64, 128],
+    )
 
-verify_lvs = False
 extracted = False
 verify_tran = True
 
@@ -40,29 +39,35 @@ if load_from_file==True:
     vh=specdict['v_in']
     vl=0.0
     vcm=specdict['v_in']/2
-    num_bits=specdict['n_bit']-1
+    params['num_bits']=specdict['n_bit']-1
+    params['c_m']=sizedict['capdac_c_m']
+    params['rdx_array']=specdict['rdx_array']
 
 print('creating BAG project')
 prj = bag.BagProject()
 
-#lvs
-if verify_lvs==True:
-    # run lvs
-    print('running lvs')
-    lvs_passed, lvs_log = prj.run_lvs(impl_lib, cell_name)
-    if not lvs_passed:
-        raise Exception('oops lvs died.  See LVS log file %s' % lvs_log)
-    print('lvs passed')
+# create design module and run design method.
+print('designing module')
+tb_dsn = prj.create_design_module(lib_name, tb_cell_name)
+print('design parameters:\n%s' % pprint.pformat(params))
+tb_dsn.design(**params)
+
+# implement the design
+print('implementing design with library %s' % impl_lib)
+tb_dsn.implement_design(impl_lib, top_cell_name=tb_cell_name, erase=True)
 
 # transfer curve test
 if verify_tran==True:
+    #print('configure TB state')
+    tb = prj.configure_testbench(impl_lib, tb_cell_name)
     # transient test
-    print('creating testbench %s__%s' % (impl_lib, tb_cell))
-    tb = prj.create_testbench(tb_lib, tb_cell, impl_lib, tester_cell_name, impl_lib)
+    #print('creating testbench %s__%s' % (impl_lib, tb_cell))
+    #tb = prj.create_testbench(tb_lib, tb_cell, impl_lib, tester_cell_name, impl_lib)
     tb.set_parameter('pvh', vh)
     tb.set_parameter('pvl', vl)
     tb.set_parameter('pvcm', vcm)
     tb.set_parameter('pper', per)
+    tb.set_parameter('tsim', per*2*(2**params['num_bits']))
 
     tb.set_simulation_environments(['tt'])
     tb.add_output("vout_tran", """getData("/O" ?result 'tran)""")
@@ -93,7 +98,7 @@ if verify_tran==True:
     dac_v=[]
     dac_code=[]
     for i, t in enumerate(tvec):
-        if t>t_next and code<2**num_bits: #256:
+        if t>t_next and code<2**params['num_bits']: #256:
             t_next+=0.5*per
             #print(code, vvec[i])
             dac_code.append(code)
